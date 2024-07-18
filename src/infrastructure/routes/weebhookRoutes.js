ba3d5b5b0bcb89acd3dd56f4a956a96f4d470498
Luis-Castillo-211220 <211220@ids.upchiapas.edu.mx>
2024-07-18 13:17:09 -0600

    [UPDATE] changes in weebhookroutes

--- src/infrastructure/routes/weebhookRoutes.js
+++ src/infrastructure/routes/weebhookRoutes.js
@@ -1,8 +1,42 @@
 const express = require('express');
 const { paypalWebhook } = require('../../infrastructure/controller/weebhookPaypal/weebhookController');
+const { getToken } = require("../Services/paypal/paypalClient")
+const { capturePayment } = require("../Services/paypal/paypalAdapter")
+const { TransactionRepository } = require("../repository/transactionRepository")
 
 const router = express.Router();
 
-router.post('/webhook/paypal', express.json(), paypalWebhook);
+
+
+router.post('/webhook/paypal', async (req, res) => {
+    const event = req.body
+
+    if(event.event_type === "CHECKOUT.ORDER.APPROVED"){
+        const paypal_payment_id = event.resource.id
+        console.log(paypal_payment_id)
+        try{
+            const response = await fetch(`https://payment-service-wdzc.onrender.com/api/v3/transactions/${paypal_payment_id}/certificate`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            if(response){
+                const data = await response.json()
+                res.status(200).json({
+                    message: 'Payment certificate generated successfully',
+                    certificate: data
+                })
+            }else{
+                res.status(400).send({message: 'Error fetching payment certificate'})
+            }
+
+        }catch(err){
+            console.error('Error:', err)
+            res.status(500).send({message: 'Error processing webhook'})
+        }
+    }
+
+});
 
 module.exports = router;
